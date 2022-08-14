8aa4c70441df148fe2931375cb8899456bccefda
2ndflight <105398722+2ndflight@users.noreply.github.com>
2022-08-13 23:32:02 -0400

    Add files via upload

---
+++ momentum/js/weather.js
@@ -0,0 +1,20 @@
+//How to call URL in JS
+const API_KEY = "48f5ce34eca8bea38b727191a4f84efb";
+
+function onGeoOK(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+navigator.geolocation.getCurrentPosition(onGeoOK, onGeoError);
